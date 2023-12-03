2fd05a7fa5bdcf46e2de9327a1d12d8f3aadc75b
Mark-E-Hahn <151473050+Mark-E-Hahn@users.noreply.github.com>
2023-12-03 15:33:20 -0600

    Add files via upload

---
+++ Individual_Info.java
@@ -0,0 +1,21 @@
+package Individual_Info;
+
+public class Individual_Info {
+
+	public static void main(String[] args) {
+		
+		String firstName = "John";
+		String lastName = "Doe";
+		String streetAddress = "123 Main St.";
+		String city = "Los Angeles";
+		String zipCode = "54321";
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipCode);
+		
+		
+	}
+}
